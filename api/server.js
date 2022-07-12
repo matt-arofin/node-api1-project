8f55b2f8c0b2ed51cace78be9896b224d749e6d9
Matthew Arofin <Matt.arofin@gmail.com>
2022-07-12 11:41:31 -0400

    9 tests passing

--- api/server.js
+++ api/server.js
@@ -18,19 +18,36 @@ server.use(express.json());
 | DELETE | /api/users/:id | Removes the user with the specified `id` and returns the deleted user.                                 |
 | PUT    | /api/users/:id | Updates the user with the specified `id` using data from the `request body`. Returns the modified user | */
 
+// GET all
+server.get('/api/users', (req, res) => {
+    // requires you to find all users
+    model.find().then(users => res.status(200).json(users))
+})
+
+// GET by id
+server.get('/api/users/:id', (req, res) => {
+    const id = req.params.id
+    model.findById(id).then(user => res.status(200).json(user))
+})
+
 // POST
 server.post('/api/users', (req, res) => {
     // Use insert function to create new user
     const body = req.body
     if(body.name  == null || body.bio == null){
-        res.status(400).json({message: 'Please provide name and bio for the user'})
+        res.status(400).json({message: 'provide name and bio for the user'})
+    } else {
+        model.insert(body).then(user => {
+            res.status(201).json(user)
+        });
     }
-    model.insert(body).then(user => {
-        res.status(201).json(user)
-    });
-    model.find(body).then
 })
 
+// DELETE
+server.delete('/api/users/:id', (req, res) => {
+    const id = req.params.id
+    model.findById(id).then(user => res.status(200).json(user))
+})
 
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+// const express = require('express')
+
 const server = require('./api/server');
 
-const port = 9000;
+const port = 5001;
+
+// server.use(express.json())
 
 server.listen(port, () => {
-    console.log('server is now listening')
+    console.log(`server is now listening on port ${port}`)
 })
 // START YOUR SERVER HERE
